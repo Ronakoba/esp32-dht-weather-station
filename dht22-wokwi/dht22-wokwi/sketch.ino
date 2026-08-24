2ec17f5d5032faa31e14508837de98ceb84d7a7c
Ronakoba <m41629612@gmail.com>
2026-08-24 22:44:36 +0530

    Update sketch.ino

--- dht22-wokwi/dht22-wokwi/sketch.ino
+++ dht22-wokwi/dht22-wokwi/sketch.ino
@@ -1,1 +1,29 @@
+#include <DHT.h>
 
+#define DHTPIN 15
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float humidity = dht.readHumidity();
+  float tempC = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(tempC)) {
+    Serial.println("Failed to read from DHT22 sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print(" %  Temperature: ");
+  Serial.print(tempC);
+  Serial.println(" °C");
+}
